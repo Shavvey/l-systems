267a75794e89f2cba4a1ee5b7140d4af7c02ee48
Cole Johnson <coledjohn@gmail.com>
2025-06-02 20:43:28 -0600

    adding very special script comments

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+# NOTE: this first line is a dumb hack mean to produce
+# compile flags for my lsp to ignore things handled by CMake
 cmake -S . -G "Unix Makefiles" -B build
+# compile the project
 cmake --build build 
+# exec final binary 
 build/l-systems
