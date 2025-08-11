f0bb94e34aaba2092fcb3d16013df489b0be989a
Cole Johnson <coledjohn@gmail.com>
2025-08-11 17:06:10 -0600

    Doing some stupid things

--- src/main.c
+++ src/main.c
@@ -9,22 +9,14 @@ int main(void) {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
   SetTargetFPS(60);
 
-  Texture2D texture = LoadTexture(ASSETS_PATH "test.png");
 
   while (!WindowShouldClose()) {
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
+    ClearBackground(BLACK);
+    Ray r = {.position = {.x = 0, .y = 1}, .direction = {.x = 3, .y = 3}};
+    DrawRay(r, WHITE);
 
-    const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-    const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-    // draw the background of the window
-    DrawTexture(texture, texture_x, texture_y, WHITE);
-
-    const char *text = "BOTTOM TEXT";
-    const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-    DrawText(text, (int) SCREEN_WIDTH / 2 -  (int) text_size.x / 2,
-             texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
     EndDrawing();
   }
